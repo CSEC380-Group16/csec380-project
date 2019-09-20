28b1c5e9eda5ee7abeea96a01103e65ac4f1c495
ajy7227 <ajy7227@rit.edu>
2019-09-20 16:33:19 -0400

    Wrote python test script to verify web page says "Hello World". Configured travis.yml for pytest. Updated requirements.txt for python libraries used in script. Fixed html to display "Hello World" instead of "Hello world".

--- tests/act2test.py
+++ tests/act2test.py
@@ -2,4 +2,15 @@
 The Python script to test if the web page displays "Hello World"
 """
 
-import pytest
+import urllib3
+from bs4 import BeautifulSoup
+
+def test():
+    http = urllib3.PoolManager()
+    response = http.request("GET", "http://localhost")
+    soup = BeautifulSoup(response.data, "html.parser")
+
+    assert soup.h1.text.strip() == "Hello World"
+
+if __name__ == '__main__':
+    test()
