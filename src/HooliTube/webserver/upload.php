076ebf55fcdef2a2e6378c65329273957252446d
MDB4241 <mdb4241@g.rit.edu>
2019-11-21 22:18:58 -0500

    Added error message to filesize being exceeded

--- src/HooliTube/webserver/upload.php
+++ src/HooliTube/webserver/upload.php
@@ -7,38 +7,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
-// Include config file
-require_once "common.php";
-
-if(isset($_POST['submit'])){
-    $name = $_FILES['file']['name'];
-    $temp = $_FILES['file']['tmp_name'];
-
-    if(move_uploaded_file($temp, "videos/".$name)){
-        echo "Uploaded!";
-        $session_id = session_id();
-        $sql = "SELECT user_id FROM sessions WHERE session_id = ?";
-        $stmt = mysqli_prepare($link,$sql);
-        mysqli_stmt_bind_param($stmt, "s", $session_id);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_store_result($stmt);
-        mysqli_stmt_bind_result($stmt, $user_id);
-        mysqli_stmt_fetch($stmt);
-
-
-
-        $sql = "INSERT INTO videos(video_name, user_id) VALUES(?, ?)";
-        $stmt = mysqli_prepare($link,$sql);
-        mysqli_stmt_bind_param($stmt ,"si", $name, $user_id);
-        mysqli_stmt_execute($stmt);
-    }
-    else{
-        echo "NOT uploaded!";
-    }
-
-
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -49,10 +17,10 @@ if(isset($_POST['submit'])){
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <link rel="stylesheet" href="css/sidebar.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
+	body{ font: 14px sans-serif; text-align: center; }
     </style>
 </head>
-<body>
+<body >
     <div class="page-header">
         <h1>Upload and contribute to our amazing selection, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>!</h1>
     </div>
@@ -70,17 +38,49 @@ if(isset($_POST['submit'])){
     <div class="main">
 
     <form action="upload.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="file" />
+	<div class="col-sm-4 col-md-offset-2">
+	<input type="file" name="file" />
+	</div>
         <input type="submit" name="submit" value="Upload" />
     </form>
 
-    <?php
-    // if(isset($_POST['submit'])){
-    //     echo "<br />".$name." has been uploaded.";
-    // }
+      <?php
+        // Include config file
+        require_once "common.php";
+            if (isset($_SERVER["CONTENT_LENGTH"])) {
+        	    if ($_SERVER["CONTENT_LENGTH"] > ((int)ini_get('post_max_size') * 1024 * 1024)) {
+                	die("<script>alert('File not uploaded, filesize exceeded 200MB!')</script>");
+            	}
+            }
+
+        if(isset($_POST['submit'])){
+            $name = $_FILES['file']['name'];
+            $temp = $_FILES['file']['tmp_name'];
 
 
-    ?>
+        if(move_uploaded_file($temp, "videos/".$name)){
+            echo "<h2 align='center'> Uploaded! </h2>";
+            $session_id = session_id();
+            $sql = "SELECT user_id FROM sessions WHERE session_id = ?";
+            $stmt = mysqli_prepare($link,$sql);
+            mysqli_stmt_bind_param($stmt, "s", $session_id);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            mysqli_stmt_bind_result($stmt, $user_id);
+            mysqli_stmt_fetch($stmt);
+
+
+            $sql = "INSERT INTO videos(video_name, user_id) VALUES(?, ?)";
+            $stmt = mysqli_prepare($link,$sql);
+            mysqli_stmt_bind_param($stmt ,"si", $name, $user_id);
+            mysqli_stmt_execute($stmt);
+        }
+        else{
+            echo "<h2 align='center'>NOT uploaded! </h2>";
+            }
+        }
+
+      ?>
 
     </div>
 
